fbffdb666c11c8fe25f2d11933bbb72679b57d41
sudarshan55 <sudarshan.sb@techinvento.com>
2021-09-17 12:09:56 +0530

    Tests removed

--- src/program-rust/src/lib.rs
+++ src/program-rust/src/lib.rs
@@ -1,3 +1,5 @@
+
+//? imports 
 use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
     log::sol_log_compute_units,
@@ -10,6 +12,8 @@ use solana_program::{
 };
 
 /// Define the type of state stored in accounts
+
+//? account extends data from this packages
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
 pub struct GreetingAccount {
     /// number of greetings
@@ -56,53 +60,3 @@ pub fn process_instruction(
     Ok(())
 }
 
-// Sanity tests
-#[cfg(test)]
-mod test {
-    use super::*;
-    use solana_program::clock::Epoch;
-    use std::mem;
-
-    #[test]
-    fn test_sanity() {
-        let program_id = Pubkey::default();
-        let key = Pubkey::default();
-        let mut lamports = 0;
-        let mut data = vec![0; mem::size_of::<u32>()];
-        let owner = Pubkey::default();
-        let account = AccountInfo::new(
-            &key,
-            false,
-            true,
-            &mut lamports,
-            &mut data,
-            &owner,
-            false,
-            Epoch::default(),
-        );
-        let instruction_data: Vec<u8> = Vec::new();
-
-        let accounts = vec![account];
-
-        assert_eq!(
-            GreetingAccount::try_from_slice(&accounts[0].data.borrow())
-                .unwrap()
-                .counter,
-            0
-        );
-        process_instruction(&program_id, &accounts, &instruction_data).unwrap();
-        assert_eq!(
-            GreetingAccount::try_from_slice(&accounts[0].data.borrow())
-                .unwrap()
-                .counter,
-            1
-        );
-        process_instruction(&program_id, &accounts, &instruction_data).unwrap();
-        assert_eq!(
-            GreetingAccount::try_from_slice(&accounts[0].data.borrow())
-                .unwrap()
-                .counter,
-            2
-        );
-    }
-}
